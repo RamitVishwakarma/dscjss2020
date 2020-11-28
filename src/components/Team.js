8a20326aee72754ebb7d38b9bd5db76b3ad986f8
naman360 <naman.geek@gmail.com>
2020-11-28 14:38:27 +0530

    style update

--- src/components/Team.js
+++ src/components/Team.js
@@ -40,7 +40,7 @@ function Team() {
           options={defaultOptions}
           height={700}
           width={700}
-          style={{ margin: "0px" }}
+          style={{ margin: "0px" ,maxWidth:'100%' }}
         />
       </div>
       <div className="team__container">
