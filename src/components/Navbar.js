87203cfca29f8280376d88c485e9574ba8153f98
naman360 <naman.geek@gmail.com>
2020-11-30 14:33:45 +0530

    menu test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,74 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from '../assets/img/logo.svg'
-import { useLocation } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/img/logo.svg";
+import { useLocation } from "react-router-dom";
+import MediaQuery from "react-responsive";
+import Sidebar from "./Sidebar";
+import { IconContext } from "react-icons";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
-    const location=useLocation();
-    return (
-        <div className="navbar__main">
-            <img src={Logo} alt=""/>
-            <div className="navbar__content">
-            <Link to="/" className={`nav__links ${location.pathname === '/' ? 'active_link' : ''}`}>Home</Link>
-            <Link to="/projects" className={`nav__links ${location.pathname === '/projects' ? 'active_link' : ''}`}>Projects</Link>
-            <Link to="/team" className={`nav__links ${location.pathname === '/team' ? 'active_link' : ''}`}>Team</Link>
-            <Link to="/events" className={`nav__links ${location.pathname === '/events' ? 'active_link' : ''}`}>Events</Link>
-            <Link to="/" className="nav__links primary-btn">Connect</Link>
+    const [isOpen,handleOpen]=useState(false);
+  const location = useLocation();
+  return (
+    <div className="navbar__main">
+      <img src={Logo} alt="" />
+      <MediaQuery maxDeviceWidth={990}>
+        {isOpen ? (
+          <IconContext.Provider value={{ className: "menu-icon close" }}>
+            <div>
+              <AiOutlineClose onClick={() => handleOpen(!isOpen)} />
             </div>
-            
-        </div>
-    )
+          </IconContext.Provider>
+        ) : (
+          <IconContext.Provider value={{ className: "menu-icon open" }}>
+            <div>
+              <AiOutlineMenu onClick={() => handleOpen(!isOpen)} />
+            </div>
+          </IconContext.Provider>
+        )}
+
+        <Sidebar />
+      </MediaQuery>
+      <div className="navbar__content">
+        <Link
+          to="/"
+          className={`nav__links ${
+            location.pathname === "/" ? "active_link" : ""
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/projects"
+          className={`nav__links ${
+            location.pathname === "/projects" ? "active_link" : ""
+          }`}
+        >
+          Projects
+        </Link>
+        <Link
+          to="/team"
+          className={`nav__links ${
+            location.pathname === "/team" ? "active_link" : ""
+          }`}
+        >
+          Team
+        </Link>
+        <Link
+          to="/events"
+          className={`nav__links ${
+            location.pathname === "/events" ? "active_link" : ""
+          }`}
+        >
+          Events
+        </Link>
+        <Link to="/" className="nav__links primary-btn">
+          Connect
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { IconContext } from "react-icons";
+import { NavLink } from "react-router-dom";
+
+
+export default function Sidebar({isOpen}) {
+  return (
+    <div className={isOpen ? "sidebar-container" : "sidebar-closed"}>
+      
+      {isOpen ? (
+        <div className="sidebar">
+          <div className="sidebar-links">
+            <NavLink exact activeClassName="selected_link" to="/">
+              Home
+            </NavLink>
+            <NavLink exact activeClassName="selected_link" to="/courses">
+              Courses
+            </NavLink>
+            <NavLink exact activeClassName="selected_link" to="/about">
+              About
+            </NavLink>
+            <NavLink exact activeClassName="selected_link" to="/contact">
+              Contact
+            </NavLink>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+}
