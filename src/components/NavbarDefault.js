5bac6f81fc85aff80cb2357ff6bfa0e48109cca5
Rucha <rucha.kasture2002@gmail.com>
2022-02-24 22:43:08 +0530

    logo updated

--- src/components/NavbarDefault.js
+++ src/components/NavbarDefault.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Logo from "../assets/img/logo.svg";
-import { useLocation } from "react-router-dom";
-import MediaQuery from "react-responsive";
-import Sidebar from "./Sidebar";
-import { IconContext } from "react-icons";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../assets/img/gdsclogo.svg';
+import { useLocation } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
+import Sidebar from './Sidebar';
+import { IconContext } from 'react-icons';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 function NavbarDefault() {
   const [isOpen, handleOpen] = useState(false);
   const location = useLocation();
   return (
-    <div className={isOpen ? "navbar__main navbar__stretch" : "navbar__main"}>
-      <img src={Logo} alt="dscjss" />
+    <div className={isOpen ? 'navbar__main navbar__stretch' : 'navbar__main'}>
+      <img src={Logo} alt='dscjss' />
       <MediaQuery maxDeviceWidth={990}>
         {isOpen ? (
-          <IconContext.Provider value={{ className: "menu-icon close" }}>
+          <IconContext.Provider value={{ className: 'menu-icon close' }}>
             <div>
               <AiOutlineClose onClick={() => handleOpen(!isOpen)} />
             </div>
           </IconContext.Provider>
         ) : (
-          <IconContext.Provider value={{ className: "menu-icon open" }}>
+          <IconContext.Provider value={{ className: 'menu-icon open' }}>
             <div>
               <AiOutlineMenu onClick={() => handleOpen(!isOpen)} />
             </div>
@@ -30,52 +30,52 @@ function NavbarDefault() {
 
         <Sidebar isOpen={isOpen} handleOpen={handleOpen} />
       </MediaQuery>
-      <div className="navbar__content">
+      <div className='navbar__content'>
         <Link
-          to="/"
+          to='/'
           className={`nav__links ${
-            location.pathname === "/" ? "active_link" : ""
+            location.pathname === '/' ? 'active_link' : ''
           }`}
         >
           Home
         </Link>
         <Link
-          to="/projects"
+          to='/projects'
           className={`nav__links ${
-            location.pathname === "/projects" ? "active_link" : ""
+            location.pathname === '/projects' ? 'active_link' : ''
           }`}
         >
           Projects
         </Link>
         <Link
-          to="/team"
+          to='/team'
           className={`nav__links ${
-            location.pathname === "/team" ? "active_link" : ""
+            location.pathname === '/team' ? 'active_link' : ''
           }`}
         >
           Team
         </Link>
         <Link
-          to="/events"
+          to='/events'
           className={`nav__links ${
-            location.pathname === "/events" ? "active_link" : ""
+            location.pathname === '/events' ? 'active_link' : ''
           }`}
         >
           Events
         </Link>
         <Link
-          to="/alumni"
+          to='/alumni'
           className={`nav__links ${
-            location.pathname === "/alumni" ? "active_link" : ""
+            location.pathname === '/alumni' ? 'active_link' : ''
           }`}
         >
           Alumni
         </Link>
         <a
-          href="https://www.instagram.com/gdscjssaten/"
-          className="nav__links primary-btn"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.instagram.com/gdscjssaten/'
+          className='nav__links primary-btn'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Connect
         </a>
