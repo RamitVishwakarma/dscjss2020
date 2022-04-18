f54875fa715d33d5d7091acf0afc9ed90118bd3a
aman123956 <amandixit033@gmail.com>
2022-04-18 19:28:55 +0530

    Fix made

--- src/components/Recruitment.js
+++ src/components/Recruitment.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import styles from './Recruitment.module.css'
 const Recruitment = () => {
   return (
-    <div className="flex__row"> 
-      <frameset rows = "10%,80%,10%" >
-        <frame name="top" src="https://gdsc-recruitments.netlify.app/" />
-        <frame name="main" src="https://gdsc-recruitments.netlify.app/" />
-        <frame name="bottom" src="https://gdsc-recruitments.netlify.app/" />
+    <div className={styles.mainContainer} > 
+      <frameset>
+        {/* <frame name="top" src="https://gdsc-recruitments.netlify.app/" /> */}
+        <frame src="https://gdsc-recruitments.netlify.app/" />
+        {/* <frame name="bottom" src="https://gdsc-recruitments.netlify.app/" /> */}
         <noframes>
           <body>Your browser does not support frames.</body>
         </noframes>
